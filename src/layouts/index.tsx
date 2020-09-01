f08a72c54262686acaf6d3bd1d4ea0d7b98a97ca
chenyou <2716698534@qq.com>
2020-09-01 10:19:01 +0800

    code

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './index.css';
 
+console.log('/index.tsx [3]--1','12');
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.normal}>
