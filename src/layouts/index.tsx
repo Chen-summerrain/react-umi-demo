f705cc6fd57da9dca2b472f848b2dce62e7b9909
chenyou <2716698534@qq.com>
2020-09-01 10:48:37 +0800

    test1-11

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -5,6 +5,7 @@ console.log('/index.tsx [3]--1','12');
 console.log('/index.tsx [3]--1','test1');
 console.log('/index.tsx [5]--1','test11');
 console.log('/index.tsx [6]--1','test2');
+console.log('/index.tsx [7]--1','test1-11');
 const BasicLayout: React.FC = props => {
   return (
     <div className={styles.normal}>
